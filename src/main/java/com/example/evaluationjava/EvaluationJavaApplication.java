16993e770ea93b4ea71b4d60558ff7ffae1b34db
leovillar07 <201614025@uns.edu.pe>
2022-05-19 00:48:24 -0500

    BASE

---
+++ src/main/java/com/example/evaluationjava/EvaluationJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.evaluationjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationJavaApplication.class, args);
+    }
+
+}
